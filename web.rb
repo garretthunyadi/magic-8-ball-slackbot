367df5082ee3517ac61197332703bd371680e78b
Garrett Hunyadi <garrett.hunyadi@endurance.com>
2015-09-04 16:40:37 -0400

    magic 8 ball

--- web.rb
+++ web.rb
@@ -10,6 +10,46 @@ TRIGGER_WORD="#"
 IMAGE_STYLE="fixed_height" # or "fixed_width" or "original"
 SLACK_POSTING_URL="https://hooks.slack.com/services/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"
 
+MAGIC_8_BALL_RESPONSES = [
+"Signs point to yes.",
+"Yes.",
+"Reply hazy, try again.",
+"Without a doubt.",
+"My sources say no.",
+"As I see it, yes.",
+"You may rely on it.",
+"Concentrate and ask again.",
+"Outlook not so good.",
+"It is decidedly so.",
+"Better not tell you now.",
+"Very doubtful.",
+"Yes - definitely.",
+"It is certain.",
+"Cannot predict now.",
+"Are you fucking kidding me???",
+"Most likely.",
+"Ask again later.",
+"My reply is no.",
+"Outlook good.",
+"Don't count on it.",
+"Yes, in due time.",
+"My sources say no.",
+"Definitely not.",
+"Yes.",
+"You will have to wait.",
+"I have my doubts.",
+"Outlook so so.",
+"Looks good to me!",
+"Who knows?",
+"Looking good!",
+"Probably.",
+"Are you kidding?",
+"Go for it!",
+"Don't bet on it.",
+"Forget about it.",
+]
+
+
 get '/' do
   "Hey there!"
 end
@@ -67,6 +107,13 @@ post '/god' do
   response = Net::HTTP.post_form(uri, {"payload" => JSON.generate(payload)})
 end
 
+post '/magic' do
+  text = "Magic 8 Ball says: '#{MAGIC_8_BALL_RESPONSES.sample}'"
+  payload={"text" => text}
+  uri = URI.parse(SLACK_POSTING_URL)
+  response = Net::HTTP.post_form(uri, {"payload" => JSON.generate(payload)})
+end
+
 
 #curl --data '{"text": "This is a line of text in a channel.\nAnd this is another line of text."}' https://hooks.slack.com/services/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
 get "/simon" do
@@ -78,8 +125,6 @@ get "/simon" do
 
   uri = URI.parse(SLACK_POSTING_URL)
   response = Net::HTTP.post_form(uri, {"payload" => JSON.generate(payload)})
-
-  return
 end
 post "/simon" do
 
@@ -119,44 +164,6 @@ post "/gif" do
 end
 
 
-MAGIC_8_BALL_RESPONSES = [
-"Signs point to yes.",
-"Yes.",
-"Reply hazy, try again.",
-"Without a doubt.",
-"My sources say no.",
-"As I see it, yes.",
-"You may rely on it.",
-"Concentrate and ask again.",
-"Outlook not so good.",
-"It is decidedly so.",
-"Better not tell you now.",
-"Very doubtful.",
-"Yes - definitely.",
-"It is certain.",
-"Cannot predict now.",
-"Are you fucking kidding me???",
-"Most likely.",
-"Ask again later.",
-"My reply is no.",
-"Outlook good.",
-"Don't count on it.",
-"Yes, in due time.",
-"My sources say no.",
-"Definitely not.",
-"Yes.",
-"You will have to wait.",
-"I have my doubts.",
-"Outlook so so.",
-"Looks good to me!",
-"Who knows?",
-"Looking good!",
-"Probably.",
-"Are you kidding?",
-"Go for it!",
-"Don't bet on it.",
-"Forget about it.",
-]
 
 
 
